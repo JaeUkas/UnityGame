307fcb4b7d9bb6c93ae3e37802026fc4d2ae0453
JaeUkas <anytime96@naver.com>
2019-05-04 17:28:26 +0900

    dodge

---
+++ Dodge/Assets/Scripts/Player/Fire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject Gm;
+    public GameObject Gm2;
+    public GameObject Bullet;
+    public Transform FirePos;
+
+    float timer;
+    float waitingTime;
+
+    private void Awake()
+    {
+        timer = 0f;
+        waitingTime = 0.2f;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > waitingTime)
+        {
+            //if (Input.GetMouseButtonDown(0))
+            {
+                transform.LookAt(new Vector3(Gm.transform.position.x, Gm.transform.position.y + 5.8f, Gm.transform.position.z));
+                //복제한다. //'Bullet'을 'FirePos.transform.position' 위치에 'FirePos.transform.rotation' 회전값으로.
+                Instantiate(Bullet, FirePos.transform.position, FirePos.transform.rotation);
+
+                transform.LookAt(new Vector3(Gm2.transform.position.x, Gm2.transform.position.y + 5.8f, Gm2.transform.position.z));
+                //복제한다. //'Bullet'을 'FirePos.transform.position' 위치에 'FirePos.transform.rotation' 회전값으로.
+                Instantiate(Bullet, FirePos.transform.position, FirePos.transform.rotation);
+            }
+            timer = 0;
+        }
+       
+    }
+
+    
+}
---
+++ Dodge/Assets/Scripts/Player/PlayerScript1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript1 : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody rigidbody;
+
+    private float h;
+    private float v;
+
+    private float moveX;
+    private float moveZ;
+    private float speedH = 800f;
+    private float speedZ = 800f;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("h", h);
+        animator.SetFloat("v", v);
+
+        moveX = h * speedH * Time.deltaTime;
+        moveZ = v * speedZ * Time.deltaTime;
+
+        rigidbody.velocity = new Vector3(moveX, 0, moveZ);
+
+    }
+}
